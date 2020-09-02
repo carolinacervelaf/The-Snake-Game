5ac9526bc38c84a93b94e70ce2049f62f2364fab
carolinacervelaf <carolinacervelaf@gmail.com>
2020-09-02 18:05:56 -0300

    .

--- index.js
+++ index.js
@@ -49,9 +49,9 @@ window.onload = function () {
   const eat = new Audio();
   const move = new Audio();
 
-  dead.src = "/Sounds/dead.wav"
-  eat.src = "/Sounds/eat.wav"
-  move.src = "/Sounds/moves.wav"
+  dead.src = "./Sounds/dead.wav"
+  eat.src = "./Sounds/eat.wav"
+  move.src = "./Sounds/moves.wav"
 
   dead.volume = 0.2;
   eat.volume = 0.2;
